b7f41f23ef543852a8f5f41c2af29339857f3a3a
Link4real <simon@die-duerrs.de>
2021-10-12 21:35:13 +0200

    a

--- APOLLO/MainWindow.xaml.cs
+++ APOLLO/MainWindow.xaml.cs
@@ -24,5 +24,75 @@ namespace APOLLO
         {
             InitializeComponent();
         }
+
+        private void ClickBtn1(object sender, RoutedEventArgs e)
+        {
+            int Btn1 = 1;
+        }
+        private void ClickBtn2(object sender, RoutedEventArgs e)
+        {
+            int Btn2 = 2;
+        }
+        private void ClickBtn3(object sender, RoutedEventArgs e)
+        {
+            int Btn3 = 3;
+        }
+        private void ClickBtn4(object sender, RoutedEventArgs e)
+        {
+            int Btn4 = 4;
+        }
+        private void ClickBtn5(object sender, RoutedEventArgs e)
+        {
+            int Btn5 = 5;
+        }
+        private void ClickBtn6(object sender, RoutedEventArgs e)
+        {
+            int Btn6 = 6;
+        }
+        private void ClickBtn7(object sender, RoutedEventArgs e)
+        {
+            int Btn7 = 7;
+        }
+        private void ClickBtn8(object sender, RoutedEventArgs e)
+        {
+            int Btn8 = 8;
+        }
+        private void ClickBtn9(object sender, RoutedEventArgs e)
+        {
+            int Btn9 = 9;
+        }
+        private void ClickBtn0(object sender, RoutedEventArgs e)
+        {
+            int Btn9 = 0;
+        }
+        private void ClickBtnBack(object sender, RoutedEventArgs e)
+        {
+        }
+        private void ClickBtnCE(object sender, RoutedEventArgs e)
+            {
+
+            }
+        private void ClickBtnEquals(object sender, RoutedEventArgs e)
+        {
+        }
+        private void ClickC(object sender, RoutedEventArgs e)
+        {
+        }
+        private void ClickGeteilt(object sender, RoutedEventArgs e)
+        {
+        }
+        private void ClickMal(object sender, RoutedEventArgs e)
+        {
+        }
+        private void ClickMinus(object sender, RoutedEventArgs e)
+        {
+        }
+        private void ClickPlus(object sender, RoutedEventArgs e)
+        {
+        }
+        private void ClickPlusMinus(object sender, RoutedEventArgs e)
+        {
+        }
     }
+    
 }
